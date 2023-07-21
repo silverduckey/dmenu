9ad88ae547eb518c5b88233bff40193873407523
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-07-21 06:53:14 +0000

    Replace config.def.h

--- config.def.h
+++ config.def.h
@@ -9,11 +9,11 @@ static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = {"#abb2bf", "#282c34"},
-    [SchemeSel] = {"#abb2bf", "#31353f"},
-    [SchemeSelHighlight] = {"#cc241d", "#31353f"},
-    [SchemeNormHighlight] = {"#cc241d", "#282c34"},
-    [SchemeOut] = {"#282c34", "#61afef"},
+    [SchemeNorm] = {"#abb2bf", "#282828"},
+    [SchemeSel] = {"#ebdbb2", "#3c3836"},
+    [SchemeSelHighlight] = {"#cc241d", "#3c3836"},
+    [SchemeNormHighlight] = {"#cc241d", "#282828"},
+    [SchemeOut] = {"#282828", "#458588"},
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines = 10;
