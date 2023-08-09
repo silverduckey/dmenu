c00f9ba8b3d27bda62f2f26e5dcf77f5a2d3b060
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-09 21:35:16 +0700

    Change some colors

--- config.def.h
+++ config.def.h
@@ -4,11 +4,11 @@ static const char *fonts[] = {"Ubuntu Nerd Font:pixelsize=14"};
 static const char *prompt = NULL;
 
 static const char *colors[SchemeLast][2] = {
-    [SchemeNorm] = {"#d8dee9", "#2e3440"},
-    [SchemeSel] = {"#d8dee9", "#3b4252"},
-    [SchemeSelHighlight] = {"#bf616a", "#3b4252"},
-    [SchemeNormHighlight] = {"#bf616a", "#2e3440"},
-    [SchemeOut] = {"#2e3440", "#81a1c1"},
+    [SchemeNorm] = {"#d8dee9", "#3b4252"},
+    [SchemeSel] = {"#88c0d0", "#4c566a"},
+    [SchemeSelHighlight] = {"#bf616a", "#4c566a"},
+    [SchemeNormHighlight] = {"#bf616a", "#3b4252"},
+    [SchemeOut] = {"#3b4252", "#81a1c1"},
 };
 
 static unsigned int lines = 10;
