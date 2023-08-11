8496a6e41c30c6c033972346dd1ac95196fe2c63
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-11 20:38:37 +0700

    Rewrite dmenu

--- config.def.h
+++ config.def.h
@@ -1,17 +1,27 @@
-static int topbar = 1;
-static int fuzzy = 1;
-static const char *fonts[] = {"Ubuntu Nerd Font:pixelsize=14"};
-static const char *prompt = NULL;
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
 
-static const char *colors[SchemeLast][2] = {
-    [SchemeNorm] = {"#d8dee9", "#3b4252"},
-    [SchemeSel] = {"#88c0d0", "#4c566a"},
-    [SchemeSelHighlight] = {"#bf616a", "#4c566a"},
-    [SchemeNormHighlight] = {"#bf616a", "#3b4252"},
-    [SchemeOut] = {"#88c0d0", "#4c566a"},
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Ubuntu Nerd Font:pixelsize=14"
 };
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d8dee9", "#3b4252" },
+	[SchemeSel] = { "#88c0d0", "#4c566a" },
+	[SchemeSelHighlight] = { "#bf616a", "#4c566a" },
+	[SchemeNormHighlight] = { "#bf616a", "#3b4252" },
+	[SchemeOut] = { "#88c0d0", "#4c566a" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 20;
+static unsigned int columns    = 5;
 
-static unsigned int lines = 20;
-static unsigned int columns = 5;
-
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
 static const char worddelimiters[] = " ";
