f6d6cd9c78b2be8569c285f7656e83e351964029
Vu Dang <silduck@proton.me>
2025-03-29 13:49:38 +0700

    Remove columns

--- config.def.h
+++ config.def.h
@@ -20,7 +20,6 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 20;
-static unsigned int columns    = 2;
 
 /*
  * Characters not considered part of a word while deleting words
